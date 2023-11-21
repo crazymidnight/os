7aceb422e761f17b1fd142bcd3009fa198a06b4d
Alexey Voytsekhovskiy <alexvoit70@gmail.com>
2023-11-21 22:45:21 +0700

    fix boot output

--- src/main.rs
+++ src/main.rs
@@ -7,12 +7,13 @@ static HELLO: &[u8] = b"Hello World!";
 
 #[no_mangle]
 pub extern "C" fn _start() -> ! {
-    let vga_buffer = 0xb0000 as *mut u8;
+    let vga_buffer = 0xb8000 as *mut u8;
+    let line_offset: isize = 160 * 2;
 
     for (i, &byte) in HELLO.iter().enumerate() {
         unsafe {
-            *vga_buffer.offset(i as isize * 2) = byte;
-            *vga_buffer.offset(i as isize * 2 + 1) = 0xb;
+            *vga_buffer.offset(i as isize * 2 + line_offset) = byte;
+            *vga_buffer.offset(i as isize * 2 + 1 + line_offset) = 0xb;
         }
     }
 
